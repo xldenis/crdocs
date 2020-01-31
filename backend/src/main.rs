3b125310fb7a57391a6acc671682f677fc906029
Xavier Denis <xldenis@gmail.com>
2020-01-31 23:53:04 +0100

    Rename front and backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,8 +9,8 @@ fn main() {
     let mut signaller = server::Signalling { clients: Vec::new() };
     use futures::future;
     let mut app = tide::new();
-    app.at("/assets/*path").get(StaticFiles::new("../crdocs_lib/www/dist"));
-    
+    app.at("/assets/*path").get(StaticFiles::new("../frontend/www/dist"));
+
     let x = async {
         println!("Starting static file server");
         app.listen("localhost:8080").await
@@ -23,6 +23,6 @@ fn main() {
 
     pin_mut!(x, s);
 
-    task::block_on(future::select(x, s)); 
+    task::block_on(future::select(x, s));
 
 }
