4889cb675c77a6832754a32a1f18716a0b6ee18d
Xavier Denis <xldenis@gmail.com>
2020-01-31 16:15:28 +0100

    start text-diffing input

--- crdocs_lib/www/index.js
+++ crdocs_lib/www/index.js
@@ -1,4 +1,21 @@
 import * as wasm from "crdocs";
 
 const p = new URLSearchParams(window.location.search);
+
+import * as diff from 'diff';
+
+const editor = document.getElementById('editor');
+editor.value = "";
+
+let prev_value = "";
+editor.oninput = function (e) { 
+  const ed = e.target;
+
+
+  let d = diff.diffChars(prev_value, editor.value);
+  console.log(d);
+  prev_value = ed.value;
+}
+
 wasm.test_webrtc_conn(p.get('id'));
+
