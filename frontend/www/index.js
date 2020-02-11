e72bf3b4a3a7144e94380ef92988694d08e6b605
Xavier Denis <xldenis@gmail.com>
2020-02-11 18:11:54 +0100

    add a fancy frontend

--- frontend/www/index.js
+++ frontend/www/index.js
@@ -4,6 +4,9 @@ const p = new URLSearchParams(window.location.search);
 
 import * as diff from 'diff';
 
+import * as mde from 'simplemde';
+
+
 const editor_div = document.getElementById('editor');
 const peers_span = document.getElementById('peers');
 editor_div.value = "";
@@ -13,31 +16,42 @@ signal_url.port = 3012
 signal_url.protocol = "ws:"
 
 wasm.create_editor(signal_url.toString()).then(function (editor) {
+  let editor_window = new mde({ element: editor_div });
+
   let prev_value = "";
-  editor_div.oninput = function (e) {
+  editor_window.codemirror.on('change', function (e) {
     const ed = e.target;
 
-
-    let d = diff.diffChars(prev_value, editor_div.value);
+    let d = diff.diffChars(prev_value, editor_window.codemirror.getValue());
     let ix = 0;
 
     for (const p of d) {
+      console.log(p);
       for (const c of p.value) {
         if (p.added) {
           editor.insert(c, ix);
+          ix++;
         } else if (p.removed) {
           editor.delete(ix);
+        } else {
+          ix++;
         }
-        ix++;
+
       }
     }
-    prev_value = ed.value;
-  }
+    prev_value = editor_window.codemirror.getValue();
+  });
 
 
   editor.onchange(function(t) {
-    editor_div.value = t;
-    prev_value = t;
+    if (t != prev_value) {
+      prev_value = t;
+      // hacky way to avoid cursor resetting on updates
+      // should really move to using replaceRange instead....
+      let selections = editor_window.codemirror.listSelections();
+      editor_window.codemirror.setValue(t);
+      editor_window.codemirror.setSelections(selections);
+    }
     peers_span.textContent = editor.num_connected_peers();
 
   });
