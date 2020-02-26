b0e60602baa16e65af1f9f007ab9733562d99ab4
Xavier Denis <xldenis@gmail.com>
2020-02-26 02:55:08 +0100

    Assign a random live peer as initial rather than minimum one

--- backend/src/server.rs
+++ backend/src/server.rs
@@ -9,6 +9,7 @@ use futures::channel::mpsc::*;
 use futures::{future, stream::{TryStreamExt, StreamExt}, pin_mut};
 
 use warp::ws::{Message, WebSocket};
+use rand::{thread_rng, seq::IteratorRandom};
 
 pub struct Signalling {
     pub clients: Vec<Client>,
@@ -32,7 +33,7 @@ use serde_json::Value;
 #[derive(Debug, Deserialize, Serialize)]
 struct SignalMessage { id: u32, msg: Value }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 enum SignalMsg {
     NewSite { },
     Reconn { site_id: u32 },
@@ -43,7 +44,8 @@ use SignalMsg::*;
 
 // Simple broadcaster taken from async-tungstenite
 pub async fn handle_connection(state: PeerMap, mut ws: WebSocket) {
-    // println!("Incoming TCP connection from: {}", addr);
+    let initial_peer = state.lock().unwrap().peers.keys().choose(&mut thread_rng()).map(|i| *i);
+
     let (tx, rx) = unbounded();
     let peer_id = {
         let txt = ws.next().await.unwrap();
@@ -57,9 +59,13 @@ pub async fn handle_connection(state: PeerMap, mut ws: WebSocket) {
                 peers.num_peers += 1;
                 peer_id
             }
+            // Only valid if the signalling server hasn't been restarted since
+            // assigning ids. TODO: Change this to use UUIDs instead so that
+            // there's no need to worry about re-assigning an id.
             Reconn { site_id } => {
                 let mut peers = state.lock().unwrap();
                 peers.peers.insert(site_id, tx);
+                peers.num_peers = peers.num_peers.max(site_id + 1);
                 site_id
             }
             _ => { return }
@@ -71,13 +77,15 @@ pub async fn handle_connection(state: PeerMap, mut ws: WebSocket) {
 
     use futures::sink::SinkExt;
 
-    let min_peer = *state.lock().unwrap().peers.keys().nth(0).unwrap_or(&0);
-
     outgoing.send(
-        Message::text(serde_json::to_string(&InitMsg { site_id: peer_id, initial_peer: min_peer }).unwrap())
+        Message::text(serde_json::to_string(&InitMsg {
+            site_id: peer_id,
+            initial_peer: initial_peer.unwrap_or(peer_id),
+
+        }).unwrap())
     ).await.unwrap();
 
-    let broadcast_incoming = incoming.try_for_each(|msg| broadcast_msg(&state, peer_id, msg));
+    let broadcast_incoming = incoming.try_for_each(|msg| unicast_msg(&state, peer_id, msg));
 
     let receive_from_others = rx.map(|msg| { log::info!("sending {} {:?}", peer_id, msg); msg}).map(Ok).forward(outgoing);
 
@@ -86,30 +94,29 @@ pub async fn handle_connection(state: PeerMap, mut ws: WebSocket) {
 
     state.lock().unwrap().peers.remove(&peer_id);
 }
-// Incredibly basic signalling server that just broadcasts every message to everyone
 
-
-async fn broadcast_msg(state: &Mutex<PeerState>, peer_id: u32, msg: Message) -> Result<(), warp::Error>{
+async fn unicast_msg(state: &Mutex<PeerState>, peer_id: u32, msg: Message) -> Result<(), warp::Error>{
     let peers = &mut state.lock().unwrap().peers;
-    let msg = msg.to_str().and_then(|i| serde_json::from_str(i).map_err(|e| log::error!("{:?}", e)));
+    if let Ok(msg) = msg.to_str() {
+        let msg = serde_json::from_str(msg);
 
-    match msg {
-        Ok(SignalMessage { id, msg }) => {
-            let msg = Message::text(serde_json::to_string(
-                    &SignalMessage { id: peer_id, msg }
-            ).unwrap());
+        match msg {
+            Ok(SignalMessage { id, msg }) => {
+                let msg = Message::text(serde_json::to_string(
+                        &SignalMessage { id: peer_id, msg }
+                ).unwrap());
 
-            if let Some(chan) = peers.get_mut(&id) {
-                chan.unbounded_send(msg).expect("send failed")
-            } else {
-                log::info!("Tried sending message to non-existent peer {}", id);
+                if let Some(chan) = peers.get_mut(&id) {
+                    chan.unbounded_send(msg).expect("send failed")
+                } else {
+                    log::info!("Tried sending message to non-existent peer {}", id);
+                }
+            }
+            Err(_) => {
+                log::error!("agggg {:?}", msg);
             }
         }
-        Err(err) => {
-            log::error!("{:?}", err);
-        }
     }
-
     Ok(())
 }
 
